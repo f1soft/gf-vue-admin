e0f7db509ff23458bf93105212318c76948fcc05
SliverHorn <503551462@qq.com>
2020-07-20 15:52:36 +0800

    Base模块api统一

--- gf-server/app/api/v1/admins.go
+++ gf-server/app/api/v1/admins.go
@@ -1,62 +1,9 @@
 package v1
 
 import (
-	"gf-server/app/api/request"
-	"gf-server/app/model/admins"
-	"gf-server/app/service"
-	"gf-server/library/global"
-	"github.com/gogf/gf/frame/g"
-
 	"github.com/gogf/gf/net/ghttp"
 )
 
-// @Tags Base
-// @Summary 用户注册账号
-// @Produce  application/json
-// @Param data body model.SysUser true "用户注册接口"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"注册成功"}"
-// @Router /base/register [post]
-func Register(r *ghttp.Request) {
-	var R request.RegisterRequest
-	if err := r.Parse(&R); err != nil {
-		global.FailWithMessage(r, err.Error())
-	}
-	u := &admins.Entity{
-		Username:    R.Username,
-		Password:    R.Password,
-		Nickname:    R.Nickname,
-		HeaderImg:   R.HeaderImg,
-		AuthorityId: R.AuthorityId,
-	}
-	if err := service.Register(u); err != nil {
-		global.FailWithMessage(r, err.Error())
-		r.ExitAll()
-	}
-	global.OkDetailed(r, g.Map{}, "注册成功!")
-}
-
-// @Tags Base
-// @Summary 用户登录
-// @Produce  application/json
-// @Param data body request.RegisterAndLoginStruct true "用户登录接口"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"登陆成功"}"
-// @Router /base/login [post]
-func Login(r *ghttp.Request) {
-	//var L request.LoginRequest
-	//_ = r.Parse(&L)
-	//if e := gvalid.CheckStruct(L, nil); e != nil {
-	//	g.Dump(e.Maps())
-	//}
-	//if store.Verify(L.CaptchaId, L.Captcha, true) {
-	//	u := &user.Entity{Username: L.Username, Password: L.Password}
-	//	userReturn, err := service.Login(u)
-	//	if err != nil {
-	//		response.FailWithMessage(r, err.Error())
-	//	}
-	//	//tokenNext(userReturn)
-	//	response.OkDetailed(r, userReturn, "登录成功!")
-	//}
-}
 
 // @Tags SysUser
 // @Summary 用户修改密码
---
+++ gf-server/app/api/v1/base.go
@@ -0,0 +1,51 @@
+package v1
+
+import (
+	"gf-server/app/api/request"
+	"gf-server/app/model/admins"
+	"gf-server/app/service"
+	"gf-server/library/global"
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+)
+
+// @Tags Base
+// @Summary 用户注册账号
+// @Produce  application/json
+// @Param data body model.SysUser true "用户注册接口"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"注册成功"}"
+// @Router /base/register [post]
+func Register(r *ghttp.Request) {
+	var R request.RegisterRequest
+	if err := r.Parse(&R); err != nil {
+		global.FailWithMessage(r, err.Error())
+	}
+	u := &admins.Entity{
+		Username:    R.Username,
+		Password:    R.Password,
+		Nickname:    R.Nickname,
+		HeaderImg:   R.HeaderImg,
+		AuthorityId: R.AuthorityId,
+	}
+	if err := service.Register(u); err != nil {
+		global.FailWithMessage(r, err.Error())
+		r.ExitAll()
+	}
+	global.OkDetailed(r, g.Map{}, "注册成功!")
+}
+
+// @Tags base
+// @Summary 生成验证码
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /base/captcha [post]
+func Captcha(r *ghttp.Request) {
+	id, b64s, err := service.Captcha()
+	if err != nil {
+		global.GFVA_LOG.Errorf("获取数据失败，err:%v", err)
+		global.FailWithMessage(r, "获取数据失败")
+	}
+	global.OkDetailed(r, g.Map{"captchaId": id, "picPath": b64s}, "验证码获取成功")
+}
