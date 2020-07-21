95f5c0cddfb4f846b040a9c8a36d25b1d54a55e2
SliverHorn <503551462@qq.com>
2020-07-21 10:33:53 +0800

    更新gf-server的sql文档

--- gf-server/document/SliverHorn.sql
+++ gf-server/document/SliverHorn.sql
@@ -4,16 +4,16 @@
 --
 create table admins
 (
-    id           int unsigned auto_increment primary key comment '自增ID',
-    create_at    timestamp                                                       null comment '创建时间',
-    update_at    timestamp                                                       null comment '更新时间',
-    delete_at    timestamp                                                       null comment '删除时间',
-    uuid         varchar(255)                                                    null comment '用户唯一标识UUID',
-    nickname     varchar(255) default 'QMPlusUser'                               null comment '用户昵称',
-    header_img   varchar(255) default 'http://www.henrongyi.top/avatar/lufu.jpg' null comment '用户头像',
-    authority_id varchar(255) default '888'                                      null comment '用户角色ID',
-    username     varchar(255)                                                    null comment '用户名',
-    password     varchar(255)                                                    null comment '用户登录密码'
+    id           int unsigned auto_increment primary key COMMENT '自增ID',
+    create_at    timestamp                                                       null COMMENT '创建时间',
+    update_at    timestamp                                                       null COMMENT '更新时间',
+    delete_at    timestamp                                                       null COMMENT '删除时间',
+    uuid         varchar(255)                                                    null COMMENT '用户唯一标识UUID',
+    nickname     varchar(255) default 'QMPlusUser'                               null COMMENT '用户昵称',
+    header_img   varchar(255) default 'http://www.henrongyi.top/avatar/lufu.jpg' null COMMENT '用户头像',
+    authority_id varchar(255) default '888'                                      null COMMENT '用户角色ID',
+    username     varchar(255)                                                    null COMMENT '用户名',
+    password     varchar(255)                                                    null COMMENT '用户登录密码'
 )
     charset = utf8;
 
@@ -26,11 +26,11 @@ create index idx_admins_deleted_at
 DROP TABLE IF EXISTS `jwts`;
 CREATE TABLE `jwts`
 (
-    `id`        int UNSIGNED                                    NOT NULL AUTO_INCREMENT comment '自增ID',
-    `create_at` timestamp                                       NULL DEFAULT NULL comment '更新时间',
-    `update_at` timestamp                                       NULL DEFAULT NULL comment '更新时间',
-    `delete_at` timestamp                                       NULL DEFAULT NULL comment '删除时间',
-    `jwt`       text CHARACTER SET utf8 COLLATE utf8_general_ci NULL comment 'jwt',
+    `id`        int UNSIGNED                                    NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `create_at` timestamp                                       NULL DEFAULT NULL COMMENT '更新时间',
+    `update_at` timestamp                                       NULL DEFAULT NULL COMMENT '更新时间',
+    `delete_at` timestamp                                       NULL DEFAULT NULL COMMENT '删除时间',
+    `jwt`       text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT 'jwt',
     PRIMARY KEY (`id`) USING BTREE,
     INDEX `idx_jwts_deleted_at` (`delete_at`) USING BTREE
 ) ENGINE = InnoDB
@@ -68,10 +68,10 @@ CREATE TABLE `apis`
 DROP TABLE IF EXISTS `casbin_rule`;
 CREATE TABLE `casbin_rule`
 (
-    `p_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '类型',
-    `v0`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '角色id',
-    `v1`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '请求路由',
-    `v2`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '请求方法',
+    `p_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类型',
+    `v0`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色id',
+    `v1`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求路由',
+    `v2`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求方法',
     `v3`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
     `v4`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
     `v5`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
@@ -86,12 +86,12 @@ CREATE TABLE `casbin_rule`
 DROP TABLE IF EXISTS `authorities`;
 CREATE TABLE `authorities`
 (
-    `authority_id`   varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL comment '角色ID',
-    `authority_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '角色名',
-    `parent_id`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '父角色ID',
-    `create_at`      datetime(0)                                             NULL DEFAULT NULL comment '创建时间',
-    `update_at`      datetime(0)                                             NULL DEFAULT NULL comment '更新时间',
-    `delete_at`      datetime(0)                                             NULL DEFAULT NULL comment '删除时间',
+    `authority_id`   varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色ID',
+    `authority_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色名',
+    `parent_id`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '父角色ID',
+    `create_at`      datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `update_at`      datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    `delete_at`      datetime(0)                                             NULL DEFAULT NULL COMMENT '删除时间',
     PRIMARY KEY (`authority_id`) USING BTREE,
     UNIQUE INDEX `authority_id` (`authority_id`) USING BTREE,
     INDEX `idx_sys_authorities_deleted_at` (`delete_at`) USING BTREE
@@ -107,21 +107,21 @@ CREATE TABLE `authorities`
 DROP TABLE IF EXISTS `menus`;
 CREATE TABLE `menus`
 (
-    `id`           int(10) UNSIGNED                                        NOT NULL AUTO_INCREMENT comment '自增ID',
-    `created_at`   timestamp(0)                                            NULL DEFAULT NULL comment '创建时间',
-    `updated_at`   timestamp(0)                                            NULL DEFAULT NULL comment '更新时间',
-    `deleted_at`   timestamp(0)                                            NULL DEFAULT NULL comment '删除时间',
-    `menu_level`   int(10) UNSIGNED                                        NULL DEFAULT NULL comment '菜单等级(预留字段)',
-    `parent_id`    int(10) UNSIGNED                                        NULL DEFAULT NULL comment '父菜单ID',
-    `path`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '路由path',
-    `name`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '路由name',
-    `hidden`       tinyint(1)                                              NULL DEFAULT NULL comment '是否在列表隐藏',
-    `component`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '前端文件路径',
-    `title`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '菜单名',
-    `icon`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL comment '菜单图标',
-    `sort`         int(255)                                                NULL DEFAULT NULL comment '排序标记',
-    `keep_alive`   tinyint(1)                                              NULL DEFAULT NULL comment '是否缓存',
-    `default_menu` tinyint(1)                                              NULL DEFAULT NULL comment '是否是基础路由(开发中)',
+    `id`           int(10) UNSIGNED                                        NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `created_at`   timestamp(0)                                            NULL DEFAULT NULL COMMENT '创建时间',
+    `updated_at`   timestamp(0)                                            NULL DEFAULT NULL COMMENT '更新时间',
+    `deleted_at`   timestamp(0)                                            NULL DEFAULT NULL COMMENT '删除时间',
+    `menu_level`   int(10) UNSIGNED                                        NULL DEFAULT NULL COMMENT '菜单等级(预留字段)',
+    `parent_id`    int(10) UNSIGNED                                        NULL DEFAULT NULL COMMENT '父菜单ID',
+    `path`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '路由path',
+    `name`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '路由name',
+    `hidden`       tinyint(1)                                              NULL DEFAULT NULL COMMENT '是否在列表隐藏',
+    `component`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '前端文件路径',
+    `title`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单名',
+    `icon`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
+    `sort`         int(255)                                                NULL DEFAULT NULL COMMENT '排序标记',
+    `keep_alive`   tinyint(1)                                              NULL DEFAULT NULL COMMENT '是否缓存',
+    `default_menu` tinyint(1)                                              NULL DEFAULT NULL COMMENT '是否是基础路由(开发中)',
     PRIMARY KEY (`id`) USING BTREE,
     INDEX `idx_menus_deleted_at` (`deleted_at`) USING BTREE
 ) ENGINE = InnoDB
@@ -137,10 +137,10 @@ CREATE TABLE `menus`
 DROP TABLE IF EXISTS `dictionaries`;
 CREATE TABLE `sys_dictionaries`
 (
-    `id`         int(10) UNSIGNED                                        NOT NULL AUTO_INCREMENT comment '自增ID',
-    `created_at` datetime(0)                                             NULL DEFAULT NULL comment '创建时间',
-    `updated_at` datetime(0)                                             NULL DEFAULT NULL comment '更新时间',
-    `deleted_at` datetime(0)                                             NULL DEFAULT NULL comment '删除时间',
+    `id`         int(10) UNSIGNED                                        NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `created_at` datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `updated_at` datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    `deleted_at` datetime(0)                                             NULL DEFAULT NULL COMMENT '删除时间',
     `name`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '字典名（中）',
     `type`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '字典名（英）',
     `status`     tinyint(1)                                              NULL DEFAULT NULL COMMENT '状态',
@@ -159,10 +159,10 @@ CREATE TABLE `sys_dictionaries`
 DROP TABLE IF EXISTS `dictionary_details`;
 CREATE TABLE `dictionary_details`
 (
-    `id`                int(10) UNSIGNED                                        NOT NULL AUTO_INCREMENT comment '自增ID',
-    `created_at`        datetime(0)                                             NULL DEFAULT NULL comment '创建时间',
-    `updated_at`        datetime(0)                                             NULL DEFAULT NULL comment '更新时间',
-    `deleted_at`        datetime(0)                                             NULL DEFAULT NULL comment '删除时间',
+    `id`                int(10) UNSIGNED                                        NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `created_at`        datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `updated_at`        datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    `deleted_at`        datetime(0)                                             NULL DEFAULT NULL COMMENT '删除时间',
     `label`             varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '展示值',
     `value`             int(11)                                                 NULL DEFAULT NULL COMMENT '字典值',
     `status`            tinyint(1)                                              NULL DEFAULT NULL COMMENT '启用状态',
@@ -175,3 +175,22 @@ CREATE TABLE `dictionary_details`
   CHARACTER SET = utf8
   COLLATE = utf8_general_ci
   ROW_FORMAT = Compact;
+
+
+-- ----------------------------
+-- Table structure for files Model
+-- ----------------------------
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `created_at` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_at` timestamp(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `deleted_at` timestamp(0) NULL DEFAULT NULL COMMENT '删除时间',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件名',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件地址',
+  `tag` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件标签',
+  `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '编号',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_file_upload_and_downloads_deleted_at`(`deleted_at`) USING BTREE,
+  INDEX `idx_exa_file_upload_and_downloads_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
